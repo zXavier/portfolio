dbf7b04a5e8d053230dec80b4b2eb4bff2e1da25
Xavier Chan <xavier.chan83@gmail.com>
2019-01-30 11:48:14 -0500

    first website commit

---
+++ javascript/budget.js
@@ -0,0 +1,78 @@
+
+document.getElementById("submitIncome").addEventListener("click",computeMonthlyIncome);
+document.getElementById("submitExpenses").addEventListener("click",computeExpenses);
+document.getElementById("incomeButton").addEventListener("click",showIncome);
+document.getElementById("expenseButton").addEventListener("click",showExpenses);/*showIncome() {
+
+  var button = document.getElementById("income");
+
+  if (button.classList.contains(".hidden")) {
+    button.classList.remove(".hidden");
+  }
+
+});*/
+
+function showIncome() {
+
+  // console.log("working");
+
+  var income = document.querySelector(".income");
+  var expense = document.querySelector(".expenses");
+
+  if (income.classList.contains("hidden")) {
+    income.classList.remove("hidden");
+    expense.classList.add("hidden");
+  }
+}
+
+function showExpenses() {
+
+  var income = document.querySelector(".income");
+  var expense = document.querySelector(".expenses");
+
+  if (expense.classList.contains("hidden")) {
+    expense.classList.remove("hidden");
+    income.classList.add("hidden");
+  }
+
+}
+
+function computeMonthlyIncome() {
+
+  var monthlyIncome;
+  var salary = parseFloat(document.getElementById("salary").value);
+
+  // console.log("Parse Float: " + parseFloat(document.getElementById("salary").value));
+
+  if (isNaN(salary)) {
+    salary = (parseFloat(document.getElementById("hourlyWage").value) *
+    parseInt(document.getElementById("weeklyHours").value)) * 52;
+
+    console.log("Salary: " + salary);
+  }
+
+  monthlyIncome = salary / 12;
+
+  if (!isNaN(parseFloat(document.getElementById("additionalIncome").value))) {
+    monthlyIncome += parseFloat(document.getElementById("additionalIncome").value);
+  }
+  console.log("Monthly Income: " + monthlyIncome);
+
+}
+
+function computeExpenses() {
+
+  var totalExpenses = 0;
+
+  var expenses = document.querySelectorAll(".expenses input");
+
+  console.log(expenses);
+
+  for (var i = 0; i < expenses.length; i++) {
+    if (!isNaN(parseFloat(expenses[i].value))) {
+      totalExpenses += parseFloat(expenses[i].value);
+      console.log("Total Expenses in loop: " + totalExpenses);
+    }
+  }
+
+}
